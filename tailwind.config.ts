216252c2092fbe5680e22915c54dd9d6689b1d70
Ojolowo <ojolowoboluwatife1@gmail.com>
2023-08-27 16:58:13 +0100

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        dark: "#000",
+        prime: "#FFB612",
+        grey98: "#989898",
+        grey33: "#333333",
+        greyBC: "#BCBCBC",
+        white: "#FFF",
+      },
+      screens: {
+        smDesktop: { max: "1024px" }, // small desktop
+        tablet: { max: "768px" },
+        mobile: { max: "480px" },
+        smMobile: { max: "320px" },
+        xs: "480px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
