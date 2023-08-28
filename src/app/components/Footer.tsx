bd60b2439b34e3bfd6e2b90b2d0a60adc298cf12
Ojolowo <ojolowoboluwatife1@gmail.com>
2023-08-28 15:22:04 +0100

    fix footer responsiveness

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,20 +13,20 @@ import { companyLinks, serviceLinks } from "../utils";
 
 export default function Footer() {
   return (
-    <footer className="relative border-t border-[#9F9F9F] pt-[116px] pb-[79px] mobile:pt-[85px] mx-auto max-w-[1920px]">
-      <div className="flex px-[2.5%] mobile:flex-col mobile:px-[17px]">
+    <footer className="relative border-t border-[#9F9F9F] pt-[116px] pb-[79px] tablet:pt-[85px] mx-auto max-w-[1920px]">
+      <div className="flex px-[2.5%] tablet:flex-col tablet:px-[17px]">
         <Logo className="mr-[96px]" />
 
-        <div className="flex-1 flex justify-around mobile:flex-col mobile:mt-[38px] mobile:gap-[53px]">
+        <div className="flex-1 flex justify-around tablet:flex-col tablet:mt-[38px] tablet:gap-[53px]">
           <div>
-            <h2 className="text-[2.2rem] font-[900] text-white mb-[32px] mobile:text-[1.7rem]">
+            <h2 className="text-[2.2rem] font-[900] text-white mb-[32px] tablet:text-[1.7rem]">
               OUR SERVICE
             </h2>
             <ul className="flex flex-col gap-[23px]">
               {serviceLinks.map((link) => (
                 <li key={link.name}>
                   <Link
-                    className="text-[2.2rem] font-medium text-white mobile:text-[1.7rem] flex items-center gap-[16px]"
+                    className="text-[2.2rem] font-medium text-white tablet:text-[1.7rem] flex items-center gap-[16px]"
                     href={link.href}
                   >
                     {link.name}
@@ -46,14 +46,14 @@ export default function Footer() {
           </div>
 
           <div>
-            <h2 className="text-[2.2rem] font-[900] text-white mb-[32px] mobile:text-[1.7rem]">
+            <h2 className="text-[2.2rem] font-[900] text-white mb-[32px] tablet:text-[1.7rem]">
               COMPANY
             </h2>
             <ul className="flex flex-col gap-[23px]">
               {companyLinks.map((link) => (
                 <li key={link.name}>
                   <Link
-                    className="text-[2.2rem] font-medium text-white mobile:text-[1.7rem]"
+                    className="text-[2.2rem] font-medium text-white tablet:text-[1.7rem]"
                     href={link.href}
                   >
                     {link.name}
@@ -63,15 +63,15 @@ export default function Footer() {
             </ul>
           </div>
 
-          <div className="mobile:hidden"></div>
+          <div className="tablet:hidden"></div>
         </div>
       </div>
 
       <hr className="border mx-[2.5%] mt-[73px] border-[#9F9F9F]" />
 
-      <h2 className="font-bold text-[2.2rem] flex gap-2 items-center justify-center text-white text-center mt-[50px] mobile:text-[1.3rem]">
-        <CopyrightBig className="mobile:hidden" />
-        <Copyright className="hidden mobile:block" />
+      <h2 className="font-bold text-[2.2rem] flex gap-2 items-center justify-center text-white text-center mt-[50px] tablet:text-[1.3rem]">
+        <CopyrightBig className="tablet:hidden" />
+        <Copyright className="hidden tablet:block" />
         <span>2023 EDDIE ENGINEER | POWERED BY POWERSHOP.LINK</span>
       </h2>
     </footer>
