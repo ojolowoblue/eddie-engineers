34fd6d4bcc28b4168d0455a226295997c80393d2
Ojolowo <ojolowoboluwatife1@gmail.com>
2023-08-28 12:58:33 +0100

    add app utilities

---
+++ src/app/utils/index.ts
@@ -0,0 +1,77 @@
+import Engineer1 from "@/app/assets/images/engineer-1.png";
+import Engineer2 from "@/app/assets/images/engineer-2.png";
+import Engineer3 from "@/app/assets/images/engineer-3.png";
+
+export const links = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Our Service",
+    href: "/service",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Hallway",
+    href: "/hallway",
+  },
+];
+
+export const services = [
+  {
+    name: "FREE STEP-BY-STEP GUIDE TO FINANCIAL LITERACY ",
+    description:
+      "Want them obsessed? Want your dream job? Dream life? This found you for a reason.",
+    image: Engineer1,
+  },
+  {
+    name: "FREE STEP-BY-STEP GUIDE TO FINANCIAL LITERACY ",
+    description:
+      "Want them obsessed? Want your dream job? Dream life? This found you for a reason.",
+    image: Engineer2,
+  },
+  {
+    name: "FREE STEP-BY-STEP GUIDE TO FINANCIAL LITERACY ",
+    description:
+      "Want them obsessed? Want your dream job? Dream life? This found you for a reason.",
+    image: Engineer3,
+  },
+];
+
+export const serviceLinks = [
+  {
+    name: "Contact Us",
+    href: "",
+  },
+  {
+    name: "Follow Us",
+    href: "",
+  },
+  {
+    name: "About",
+    href: "",
+  },
+];
+
+export const companyLinks = [
+  {
+    name: "Privacy Policy",
+    href: "",
+  },
+  {
+    name: "Terms of Conditions",
+    href: "",
+  },
+  {
+    name: "Shipping & Return policy",
+    href: "",
+  },
+  {
+    name: "Discover more store",
+    href: "",
+  },
+];
