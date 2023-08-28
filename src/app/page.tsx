7f248349a5e5bdd58e5a47007083196ffb99ce57
Ojolowo <ojolowoboluwatife1@gmail.com>
2023-08-28 13:01:06 +0100

    bring it all together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,79 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+
+import HeroImage from "@/app/assets/images/hero-image.png";
+import HeroImageMobile from "@/app/assets/images/here-image-mobile.png";
+import ChevronLeft from "@/app/assets/svg/swipe-left.svg";
+import ChevronRight from "@/app/assets/svg/swipe-right.svg";
+
+import Header from "./components/Header";
+import Button from "./components/Button";
+import ServiceCard from "./components/ServiceCard";
+import Footer from "./components/Footer";
+import { services } from "./utils";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className="relative px-[2.5%] mobile:px-[17px] bg-dark tablet:px-[17px] mx-auto max-w-[1920px] pt-[46px]">
+        <Header />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="relative flex flex-col justify-end h-[839px] tablet:h-[605px] mt-[48px] tablet:mt-[22px] bg-dark">
+          <div className="text-white z-40 px-[54px] tablet:px-[38px] mb-[67px] tablet:mb-[35px]">
+            <h1 className="text-[7.9rem] font-[900] max-w-[595px] tablet:text-[3.5rem]">
+              Eddie Engineer Store
+            </h1>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <p className="text-[2.4rem] font-medium max-w-[527px] tablet:text-[1.5rem]">
+              We are helping you with all your engineering need
+            </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <Button className="w-[305px] rounded-[20px] h-[95px] mt-[25px] text-[3.1rem] tablet:w-[176px] px-[39px] text-dark tablet:text-[1.8rem] tablet:h-[56px] tablet:px-[16px] tablet:rounded-[12px]">
+              Learn more
+            </Button>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <Image
+            src={HeroImage}
+            alt="Engineer Lady"
+            className="absolute top-0 h-full w-full z-20 tablet:hidden"
+          />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Image
+            src={HeroImageMobile}
+            alt="Engineer Lady"
+            className="absolute top-0 h-full w-full hidden z-20 tablet:block"
+          />
+
+          <div className="absolute top-0 h-full w-full bg-transparentDark z-30" />
+        </section>
+
+        <section className="my-[80px]">
+          <div className="flex items-center justify-between mb-[46px]">
+            <h1 className="text-[4.3rem] mobile:text-[2.8rem] font-[900] text-white">
+              Our Service
+            </h1>
+
+            <div className="flex items-center">
+              <ChevronLeft />
+              <ChevronRight />
+            </div>
+          </div>
+
+          <div className="flex gap-[32px] mobile:gap-[20px] overflow-x-auto no-scrollbar">
+            {services.map((service, idx) => (
+              <ServiceCard
+                name={service.name}
+                description={service.description}
+                image={service.image}
+                key={idx.toString()}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
